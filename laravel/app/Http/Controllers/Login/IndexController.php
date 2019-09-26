c5e3427780ab9824584de730a24057ab29e91522
1344418722@qq.com <1344418722@qq.com>
2019-09-26 16:51:22 +0800

    123

--- laravel/app/Http/Controllers/Login/IndexController.php
+++ laravel/app/Http/Controllers/Login/IndexController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\Login;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 use App\Model\Login;
 use Illuminate\Support\Facades\Session;
 
